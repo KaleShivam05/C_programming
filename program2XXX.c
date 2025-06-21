a0fa0226f2344e60e167ec88045822bfa926871c
KaleShivam05 <shivamkale0510@gmail.com>
2025-06-21 18:06:13 +0530

    Update program2XXX.c

--- program2XXX.c
+++ program2XXX.c
@@ -1,6 +1,6 @@
 ///////////////////////////////////////////////////////
 //
-//  File name :     program2.c
+//  File name :     program2XXX.c
 //  Descreption :   Used to calculate percentage
 //  Input :         Integer
 //  Output :        Integer, String

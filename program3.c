036c3c391b695f9026a5fd052c99be7327392ba1
KaleShivam05 <shivamkale0510@gmail.com>
2025-06-21 18:06:42 +0530

    Update program3.c

--- program3.c
+++ program3.c
@@ -1,6 +1,6 @@
 ///////////////////////////////////////////////////////
 //
-//  File name :     program2.c
+//  File name :     program3.c
 //  Descreption :   Used to calculate percentage
 //  Input :         Integer
 //  Output :        String

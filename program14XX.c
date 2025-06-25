6b2dac5b90b3aff35348d55d2f90bb60e1aab03d
KaleShivam05 <shivamkale0510@gmail.com>
2025-06-25 23:04:20 +0530

    Add files via upload

---
+++ program14XX.c
@@ -0,0 +1,59 @@
+/////////////////////////////////////////////////////////////////
+//
+//  File name :     program14XX.c
+//  Descreption :   Display factorial of numbers
+//  Input :         Integer
+//  Output :        Integer, String
+//  Author :        Shivam Rajendra Kale
+//  Date :          22/06/2025
+//
+//////////////////////////////////////////////////////////////////
+
+/*
+    Start
+        Accept number from user
+         Display factorial of numbers    
+    Stop
+*/
+
+#include<stdio.h>
+
+#define ERR_INVALID -1
+
+int Factorial(unsigned int iNo)
+{
+    int iCnt = 0, iFact = 1;
+
+    if(iNo < 0)
+    {
+        return ERR_INVALID;
+    }
+
+    for(iCnt = 1; iCnt <= iNo; iCnt++)
+    {
+        iFact = iFact * iCnt;
+    }
+
+    return iFact;
+}
+
+int main()
+{
+    unsigned int iValue = 0;
+    int iRet = 0;
+    
+    printf("Enter number : \n");
+    scanf("%u",&iValue);
+
+    iRet = Factorial(iValue);
+    if(iRet == ERR_INVALID)
+    {
+        printf("Invalid input\n");
+    }
+    else
+    {
+        printf("Factorial is : %d\n",iRet);
+    }
+    
+    return 0;
+} 

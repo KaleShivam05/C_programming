1ecb3820c3d8bd10fe0134b483ba58bbadf12b47
KaleShivam05 <shivamkale0510@gmail.com>
2025-06-29 22:44:27 +0530

    Add files via upload

---
+++ program36.c
@@ -0,0 +1,40 @@
+/////////////////////////////////////////////////////////////////
+//
+//  File name :     program36.c
+//  Descreption :   Display number
+//  Author :        Shivam Rajendra Kale
+//  Date :          29/06/2025
+//
+//////////////////////////////////////////////////////////////////
+
+//sequance
+
+#include<stdio.h>
+
+void Display()
+{
+    printf("*\t");
+    printf("*\t");
+    printf("*\t");
+    
+    printf("\n");
+
+    printf("*\t");
+    printf("*\t");
+    printf("*\t");
+    
+    printf("\n");
+    printf("*\t");
+    printf("*\t");
+    printf("*\t");
+    
+    printf("\n");
+}
+
+int main()
+{
+
+    Display();
+
+    return 0;
+}

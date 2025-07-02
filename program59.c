fc1adb516d443876ff209fc7e48eae70b5edcabb
KaleShivam05 <shivamkale0510@gmail.com>
2025-07-02 19:17:02 +0530

    Add files via upload

---
+++ program59.c
@@ -0,0 +1,68 @@
+/////////////////////////////////////////////////////////////////
+//
+//  File name :     program59.c
+//  Descreption :   Display summation of Even number 
+//  Input :         Integer
+//  Output :        Integer
+//  Author :        Shivam Rajendra Kale
+//  Date :          02/07/2025
+// 
+//////////////////////////////////////////////////////////////////
+
+/*
+    Start 
+        Accept Input from user
+            Display summation of Even number 
+    Stop        
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int SumEven(int Arr[], int iSize)
+{
+    int iCnt = 0, iSum = 0;
+
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        if((Arr[iCnt] % 2) == 0)
+        {
+            iSum = iSum + Arr[iCnt];
+        }
+    }
+
+    return iSum;
+}
+
+int main()
+{
+    int iLength = 0, iCnt = 0, iRet = 0;
+    int *ptr = NULL;
+
+    printf("Enter the number of elements : \n");
+    scanf("%d",&iLength);
+
+    // Step 1 : Allocate the memory
+    ptr = (int *)malloc(iLength * sizeof(int));
+    if(ptr == NULL)
+    {
+        printf("Unable to allocate memory\n");
+        return -1;
+    }
+
+    printf("Enter the elements : \n");
+    for(iCnt = 0; iCnt < iLength; iCnt++)
+    {
+        scanf("%d",&ptr[iCnt]);
+    }
+
+    // Step 2 : Use that memory
+    iRet = SumEven(ptr,iLength);
+    
+    printf("Summation of even elements is : %d\n",iRet);
+
+    // Step 3 : Free the memory 
+    free(ptr);
+
+    return 0;
+}

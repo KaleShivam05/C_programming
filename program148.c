e15a4f335a00eb394dab9aeb705659670dcc72f3
KaleShivam05 <shivamkale0510@gmail.com>
2025-07-08 16:15:19 +0530

    Add files via upload

---
+++ program148.c
@@ -0,0 +1,23 @@
+/////////////////////////////////////////////////////////////////
+//
+//  File name :     program148.c
+//  Descreption :   read from file
+//  Output :        String
+//  Author :        Shivam Rajendra Kale
+//  Date :          08/07/2025
+// 
+//////////////////////////////////////////////////////////////////
+
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<string.h>
+
+int main()
+{
+    write(1,"Jay Ganesh...",13);
+
+    return 0;
+}

8348b2bd0f490a3d3e60793c9ff6e561d45b0227
KaleShivam05 <shivamkale0510@gmail.com>
2025-07-08 16:14:43 +0530

    Add files via upload

---
+++ program143.c
@@ -0,0 +1,46 @@
+/////////////////////////////////////////////////////////////////
+//
+//  File name :     program143.c
+//  Descreption :   read from file
+//  Input :         String (file name)
+//  Author :        Shivam Rajendra Kale
+//  Date :          08/07/2025
+// 
+//////////////////////////////////////////////////////////////////
+
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<string.h>
+
+int main()
+{
+    char Fname[20] = {'\0'};
+    int fd = 0, iRet = 0;
+    char Buffer[50] = {'\0'};
+
+    printf("Enter the file name that you want to open : \n");
+    scanf("%s",Fname);
+
+    fd = open(Fname, O_RDONLY);        // Change
+    if(fd == -1)
+    {
+        printf("Unable to open file\n");
+    }
+    else
+    {
+        printf("File is succesfully opened with fd : %d\n",fd);
+        
+        iRet = read(fd,Buffer,11);
+
+        printf("%d bytes gets read sucessfully\n",iRet);
+
+        printf("Data from file is : %s\n",Buffer);
+        
+        close(fd);
+    }
+
+    return 0;
+}
